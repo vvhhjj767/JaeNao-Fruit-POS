44cab13a630059921a7dd752d97363748725fcee
vvhhjj767 <vvhhjj767@gmail.com>
2026-03-29 16:27:05 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+// ตั้งค่าระบบ
+const API_URL = 'https://script.google.com/macros/s/AKfycbxxt581KLjOjPSfuuyo4qy1j5PKcO1QZXH-H7kkZZMuq1GnpiiXq9uMi1lGja_ELIhv/exec';
+const MY_SECRET = "My_Super_Secret_Password_999";
+const SHOP_PROMPTPAY_ID = "0811147477"; // เปลี่ยนเป็นเบอร์พร้อมเพย์ หรือ เลขบัตร ปชช. ของร้าน
