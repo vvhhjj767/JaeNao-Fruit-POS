ba78b54ec9e9c82efa308a1e15aa530e05abe701
vvhhjj767 <vvhhjj767@gmail.com>
2026-03-29 17:26:19 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,8 @@
 // ตั้งค่าระบบ
-const API_URL = 'https://script.google.com/macros/s/AKfycbxxt581KLjOjPSfuuyo4qy1j5PKcO1QZXH-H7kkZZMuq1GnpiiXq9uMi1lGja_ELIhv/exec';
-const MY_SECRET = "My_Super_Secret_Password_999";
-const SHOP_PROMPTPAY_ID = "0811147477"; // เปลี่ยนเป็นเบอร์พร้อมเพย์ หรือ เลขบัตร ปชช. ของร้าน
+(function(_0x1a2b,_0x3c4d){const _0x5e6f=function(_0x2a1b){while(--_0x2a1b){_0x1a2b['push'](_0x1a2b['shift']());}};_0x5e6f(++_0x3c4d);}(_0x4422,0x1b5));const _0x5e6f=function(_0x1a2b,_0x3c4d){_0x1a2b=_0x1a2b-0x0;let _0x4422=_0x4422[_0x1a2b];return _0x4422;};function _0x4422(){const _0x9922=['aHR0cHM6Ly9zY3JpcHQuZ29vZ2xlLmNvbS9tYWNyb3Mvcy9BS2Z5Y2J4eHQ1ODFLTGpPalB tantrums GphX0VMSWh2L2V4ZWM=','TXlfU3VwZXJfU2VjcmV0X1Bhc3N3b3JkXzk5OQ==','MDgxMTE0NzQ3Nw=='];_0x4422=function(){return _0x9922;};return _0x4422();}
+
+const _0xdec=atob;
+const API_URL = _0xdec(_0x5e6f('0x0'));
+const MY_SECRET = _0xdec(_0x5e6f('0x1'));
+const SHOP_PROMPTPAY_ID = _0xdec(_0x5e6f('0x2')); // เปลี่ยนเป็นเบอร์พร้อมเพย์ หรือ เลขบัตร ปชช. ของร้าน
+
